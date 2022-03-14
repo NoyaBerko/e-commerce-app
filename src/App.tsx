bb948a0bdae8d8052ef3ff7b5c1813086d5b20e1
NoyaBerko <89184290+NoyaBerko@users.noreply.github.com>
2022-03-14 13:15:42 +0200

    updated pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,29 @@
-import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Cart from './pages/Cart';
+import Category from './pages/Category';
+import Checkout from './pages/Checkout';
+import Favorites from './pages/Favorites';
+import Home from './pages/Home';
+import Product from './pages/Product';
+import UserAccount from './pages/UserAccount';
 
-const App = () => <div className="App" />;
+const App = () => {
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path="/category" element={<Category />} />
+                    <Route path="/checkout" element={<Checkout />} />
+                    <Route path="/favorites" element={<Favorites />} />
+                    <Route path="/product" element={<Product />} />
+                    <Route path="/userAccount" element={<UserAccount />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+};
 
 export default App;
