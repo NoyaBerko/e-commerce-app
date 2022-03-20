810582c087c54c5058bf86d24f00222489ecdf13
NoyaBerko <89184290+NoyaBerko@users.noreply.github.com>
2022-03-20 18:10:14 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Product from './pages/Product';
 import UserAccount from './pages/UserAccount';
 
 const App = () => {
+    const test = 1;
     return (
         <div className="App">
             <BrowserRouter>
